6b62ce07e3510a453541c496dab209d76ceee2e9
Erika Rivera <rivera.erika07@gmail.com>
2022-08-31 14:08:34 -0400

    updated files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import Todos from './components/Todos';
+import Todo from './components/Todo'
+import Edit from './components/EditTodo'
+
+// import { Route, Switch, NavLink, Link } from 'react-router-dom'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>ToDo List</h1>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Todos />} />
+          <Route path='/:id' element={<Todo />} />
+          <Route path='/:id/edit' element={<Edit />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Todo.js
@@ -0,0 +1,34 @@
+import { getTodo, deleteTodo } from '../services/todos-api'
+import { useState, useEffect } from 'react'
+import { useParams, useNavigate, Link } from 'react-router-dom'
+
+
+function Todo() {
+    const nav = useNavigate()
+    const { id } = useParams()
+    const [todo, setTodo] = useState({})
+    useEffect(() => {
+        getTodo(id)
+        .then(res => setTodo(res.data))
+    }, [])
+
+    const deleteTheTodo = () => {
+        deleteTodo(id) // service in todos-api
+        nav('/') // take us back to home screen
+    }
+
+    return (
+        <div>
+            <h1>Todo:</h1>
+            <h3>{todo.description}</h3>
+            Completed: <input type='checkbox' checked={todo.complete} />
+            <br />
+            <button onClick={() => {nav(`/${id}/edit`)}}>Edit</button>
+            <button onClick={deleteTheTodo}>Delete</button>
+            <br />
+            <Link to='/'> Go Back to main</Link>
+        </div>
+    )
+}
+
+export default Todo
