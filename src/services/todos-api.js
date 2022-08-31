81512a62760a3ec52e1aad1701aa9ef83388f868
Erika Rivera <rivera.erika07@gmail.com>
2022-08-31 15:35:47 -0400

    update URL

--- src/services/todos-api.js
+++ src/services/todos-api.js
@@ -1,32 +1,37 @@
 // This file will be our endpoints
 import axios from 'axios'
+const baseURL ="https://erikas-express-todo.herokuapp.com/todos"
 
 export const getTodos = () => {
-    const URL = 'http://localhost:3001/todos'
+    // const URL = 'https://erikas-express-todo.herokuapp.com/todos'
+    const URL = baseURL
     const response = axios.get(URL)
     return response
 }
 
 export const getTodo = (id) => {
-    const URL = `http://localhost:3001/todos/${id}`
+    // const URL = `https://erikas-express-todo.herokuapp.com/todos/${id}`
+    const URL = `${baseURL}/${id}`
     const response = axios.get(URL)
     return response
 }
 
 export const deleteTodo = (id) => {
-    const URL = `http://localhost:3001/todos/${id}`
+    // const URL = `https://erikas-express-todo.herokuapp.com/todos/${id}`
+    const URL = `${baseURL}/${id}`
     const response = axios.delete(URL)
     return response
 }
 
 export const editTodo = (id, updatedTodo) => {
-    const URL = `http://localhost:3001/todos/${id}`
+    // const URL = `https://erikas-express-todo.herokuapp.com/todos/${id}`
     const response = axios.put(URL, updatedTodo)
     return response
 }
 
 export const createTodo = (todo) => {
-    const URL = 'http://localhost:3001/todos'
+    // const URL = 'https://erikas-express-todo.herokuapp.com/todos'
+    const URL = baseURL
     const response = axios.post(URL, todo)
     return response
 }
